fb1e864ccbf13e840c1816ab5d25c46db65cb603
Дима <@DESKTOP-TC925PR>
2023-05-07 16:39:06 +0300

    Add project files.

---
+++ dz 26.04/Systemizer.cs
@@ -0,0 +1,25 @@
+﻿using homework_26._04._2023.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz26_04
+{
+    internal class Systemizer
+    {
+        public string GetBook(string bookName)
+        {
+            Database db = new Database();
+            Book bk = db.Find(bookName);
+
+            if (bk != null)
+            {
+                return bk.ToString();
+            }
+            return "Книга не найдена!";
+        }
+    }
+
+}
